89bc03c6c2c54be195f4d0499e538ba9713b036f
anitinbhagat <anitinbhagat@gmail.com>
2026-08-02 18:37:34 +0530

    Updated private IP adresses of Jenkins master and slave ec2

--- terraform_files/taxi-infra.tf
+++ terraform_files/taxi-infra.tf
@@ -189,7 +189,7 @@ resource "aws_instance" "jenkins_master" {
   vpc_security_group_ids = [aws_security_group.demo-sg.id]
 
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
-
+te
   tags = {
     Name = "jenkins-master"
   }
